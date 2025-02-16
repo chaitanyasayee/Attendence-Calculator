802d261423ab9c5758133b835b0493a872e8b9fc
Padmanabha Chaitanya Sayee <chaitanyapsayee@gmail.com>
2025-02-16 18:54:56 +0530

    main app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+import AttendState from "./contexts/AttendContext";
+import AlertState from "./contexts/AlertContext";
+import AuthState from "./contexts/AuthContext";
+import ReviewState from "./contexts/ReviewContext";
+import Navbar from "./components/Navbar";
+import Subjects from "./components/Subjects";
+import Footer from "./components/Footer";
+import Alert from "./components/Alert";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Reviews from "./components/Reviews";
+import Splash from "./components/Splash";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [progress, setProgress] = useState(0);
+
+  const [showComponent, setShowComponent] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowComponent(true);
+    }, 4000);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <AttendState setProgress={setProgress}>
+      <AlertState>
+        <Router>
+          <AuthState setProgress={setProgress}>
+            <ReviewState setProgress={setProgress}>
+              {showComponent?<>
+              <LoadingBar color="#f8cf38" progress={progress} height={4} />
+              <Navbar />
+              <Alert />
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/attendance" element={<Subjects />} />
+                <Route
+                  exact
+                  path="/signup"
+                  setProgress={setProgress}
+                  element={<Signup />}
+                />
+                <Route
+                  exact
+                  path="/login"
+                  setProgress={setProgress}
+                  element={<Login />}
+                />
+                <Route
+                  exact
+                  path="/reviews"
+                  setProgress={setProgress}
+                  element={<Reviews />}
+                />
+              </Routes>
+              <Footer /></>:<Splash />}
+            </ReviewState>
+          </AuthState>
+        </Router>
+      </AlertState>
+    </AttendState>
+  );
+};
 
-export default App
+export default App;
